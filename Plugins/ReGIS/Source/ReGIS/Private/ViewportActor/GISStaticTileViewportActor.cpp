503dfd40b26f358e61dbf1ad4bdfb482585a3b2f
KUMAR MANISH SINGH <59444327+th-efool@users.noreply.github.com>
2025-10-05 13:06:42 +0530

    Refactor: init
    Refactoring Rendering Logic in seperate actor component, instead of concentrating everything in GISStaticViewPortActor
    
    GISStaticVIewPortActor would only be responbile for handling inputs
    
    We'll create&attach overlay component to display navigational path

--- Plugins/ReGIS/Source/ReGIS/Private/ViewportActor/GISStaticTileViewportActor.cpp
+++ Plugins/ReGIS/Source/ReGIS/Private/ViewportActor/GISStaticTileViewportActor.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "GISStaticTileViewportActor.h"
+#include "ViewportActor/GISStaticTileViewportActor.h"
 #include "DBMS/DataManager.h"
 #include "Utils/GISErrorHandler.h"
 #include "Utils/GISConversionEngine.h"
